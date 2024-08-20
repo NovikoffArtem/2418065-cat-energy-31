b20273b35a0ae07062a4394f672c7d96c93f3ab7
NovikoffArtem <142929498+NovikoffArtem@users.noreply.github.com>
2024-08-20 06:45:11 +0300

    Update script.js

--- source/scripts/script.js
+++ source/scripts/script.js
@@ -47,5 +47,5 @@ function shiftТheСurtain (event) {
 }
 
 function stopTheCurtainShifting () {
-  window.removeEventListener('touchmove', shiftТheСurtain);
+ window.removeEventListener('touchmove', shiftТheСurtain);
 }
